bd2c47744e9dc388607209482f36c32044fc1307
Pedro Garcias <pedro.garcias@gmail.com>
2020-07-15 20:40:56 -0300

    performace model

--- streamlit/getgames.py
+++ streamlit/getgames.py
@@ -5,19 +5,25 @@ from src.get_inputs import get_my_games
 from src.add_odds import add_odds_next
 import pandas as pd
 from model.ger_model import predict_proba
+from model.model_performance import check_performance
+import warnings
+warnings.filterwarnings('ignore')
 
 def main():
     params=Params()
     leagues=params.leagues
     df = pd.DataFrame()
     for league in leagues:
-        df_league=get_my_games(params.link_leagues[league],params)
-        if df_league.shape[0] == 0:
+        try:
+            df_league=get_my_games(params.link_leagues[league],params)
+            if (df_league.shape[0] == 0) or (check_performance(league,params)==False):
+                pass
+            else:
+                df_league=add_odds_next(df_league,league,params)
+                df_league=predict_proba(df_league,league,params)
+                df=pd.concat([df,df_league])
+        except:
             pass
-        else:
-            df_league=add_odds_next(df_league,league,params)
-            df_league=predict_proba(df_league,league,params)
-            df=pd.concat([df,df_league])
     df=df.reset_index(drop=True)
     df.to_csv('preview.csv',index=False)
 
